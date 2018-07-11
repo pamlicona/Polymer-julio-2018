8dca40ed92be7f0df60b5d9e579602addd15faef
pamlicona <pamela_licona@live.com>
2018-07-11 14:10:05 -0500

    Modularizer

--- pamlicona/ec6/script.js
+++ pamlicona/ec6/script.js
@@ -1,17 +1,23 @@
 const progessBar = document.querySelector('.progress__bar')
 const imageBar = document.querySelector('.image')
-let progress = 0
+var progress = 0
+
+let getRandom = (interval) => Math.random() * interval
+
+let addImage = () => {
+  clearInterval(startProgress)
+  imageBar.innerHTML = `<div><img src="img/finish.jpeg"></div>`
+}
 
 let updateProgressbar = () => {
   if (progress >= 100) {
-    clearInterval(startProgress)
-    imageBar.innerHTML = `<div><img src="img/finish.jpeg"></div>`
+    addImage()
   } else {
-    progress += Math.random() * 20
+    progress += getRandom(20)
     progessBar.style.width = progress + '%'
   }
 }
 
 let startProgress = () => {
-  setInterval(updateProgressbar, Math.random() * 2000)
+  setInterval(updateProgressbar, getRandom(2000))
 }
