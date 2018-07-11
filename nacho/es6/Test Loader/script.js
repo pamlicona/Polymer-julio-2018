48e337528fdd554ab09f3dabbf618db31c43dcce
ivillarreal <iva_arcangel@hotmail.com>
2018-07-11 14:10:57 -0500

    Loader cambio de var por let

--- nacho/es6/Test Loader/script.js
+++ nacho/es6/Test Loader/script.js
@@ -35,9 +35,9 @@ function changeColor(){
 }
 
 function getRandomColor() {
-  var letters = '0123456789ABCDEF';
-  var color = '#';
-  for (var i = 0; i < 6; i++) {
+  let letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
     color += letters[Math.floor(Math.random() * 16)];
   }
   return color;
