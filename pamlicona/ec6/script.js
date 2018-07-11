9dfa5478f957547da2ac3b03ee3c593cac5c2756
pamlicona <dyll240719gmail.com>
2018-07-11 07:33:40 -0500

    Add image when finish

--- pamlicona/ec6/script.js
+++ pamlicona/ec6/script.js
@@ -1,12 +1,14 @@
 const progessBar = document.querySelector('.progress__bar')
+const imageBar = document.querySelector('.image')
 let progress = 0
 
 let updateProgressbar = () => {
-  if (progress < 100) {
+  if (progress >= 100) {
+    clearInterval(startProgress)
+    imageBar.innerHTML = `<div><img src="img/finish.jpeg"></div>`
+  } else {
     progress += Math.random() * 20
     progessBar.style.width = progress + '%'
-  } else {
-    clearInterval(startProgress)
   }
 }
 
